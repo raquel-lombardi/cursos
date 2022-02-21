37b9be4b7c5580cb12e509a54606854dc1d9b4d0
raquel-lombardi <r.lombardif@gmail.com>
2022-02-21 14:13:04 -0300

    Anotações cursos cataline.js

---
+++ cataline/anotacoes.js
@@ -0,0 +1,31 @@
+// primitive variables
+
+var name ="Raquel Lombardi" //string
+var age =26 //integer (int)
+var weight = 66.5 //float
+var alive = true // boolean
+var food = null // null 
+var time // undefined
+
+
+// variáveis compostas
+
+var fruits =['',23,66.7,false,null] //pode ter vários
+
+var fruits = ['morango','maça','abacate'] // tipo array
+
+//objeto
+var person = {
+    name: 'Raquel lombardi', //isso é uma propriedade
+    age: 26,
+    alive:true
+}
+
+console.log(person)
+
+//concatenação
+
+var product ='camisa'
+var price =10
+
+console.log('O preço de' + product + 'é de R$ ' +price)
